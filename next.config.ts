d33bf3b81b20030bc84f46f13e0160a598802a8e
steven.vos <steven.vos@iodigital.com>
2025-05-26 14:15:22 +0200

    feat(config): add production url to remote-patterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	images: {
-		remotePatterns: [new URL("https://canny-mammoth-690.convex.cloud/**")],
+		remotePatterns: [
+			new URL("https://canny-mammoth-690.convex.cloud/**"),
+			new URL("https://determined-skunk-362.convex.cloud/**"),
+		],
 	},
 };
 
